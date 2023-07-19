2f161322ad6b1550e4de1db8aa81a3ecc5fe87dd
“mr-anye” <writetoanye@gmail.com>
2023-07-19 14:08:40 -0700

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ * Return: Returns 0 when successful
+ */
+int main(void)
+{
+	int numb = 0;
+
+	while (numb <= 10)
+	{
+		printf("%d", numb);
+		numb++;
+	}
+
+	return (0);
+
+}
